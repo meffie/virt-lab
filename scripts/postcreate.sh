07593f4ff4ac175910e6de2917862570d256dc70
Michael Meffie <mmeffie@sinenomine.net>
2019-10-17 16:45:19 -0400

    Fix syntax error in sample postcreate script

--- scripts/postcreate.sh
+++ scripts/postcreate.sh
@@ -2,12 +2,12 @@
 set -e
 set -x
 
-@echo "Running post-create script for lab ${VIRTLAB_NAME}."
+echo "Running post-create script for lab ${VIRTLAB_NAME}."
 
 # Resolve the guest names with libvirt's dnsmasq. Note: The libvirt network
 # must be setup to use a remote name resolver to avoid resolver loops.
-@echo "Set virtual dns server"
+echo "Set virtual dns server"
 sudo systemd-resolve --interface ${VIRTLAB_BRIDGE} --set-dns ${VIRTLAB_GATEWAY} --set-domain ${VIRTLAB_DOMAIN}
 
-@echo "Wait for guests to start"
+echo "Wait for guests to start"
 ansible-playbook -i ${VIRTLAB_SCRIPTDIR}/inventory.sh ${VIRTLAB_PLAYBOOKDIR}/wait.yaml
